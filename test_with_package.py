512e0bc53ec6585c5b706e0fc0a9a141685ba6f6
TobyGE <15188313009@163.com>
2017-12-10 15:43:11 -0500

    update6

--- test_with_package.py
+++ test_with_package.py
@@ -2,19 +2,23 @@
 #author : GE
 
 import numpy as np
+import scipy.cluster.vq as vq
 from sklearn.cluster import KMeans
 data = np.loadtxt(open("input.csv","rb"),delimiter=",",skiprows=0)
 
 print(type(data))
 
-estimator = KMeans(n_clusters=40,max_iter=5000)#构造聚类器
-estimator.fit(data)#聚类
-label_pred = estimator.labels_ #获取聚类标签
-print(label_pred)
-centroids = estimator.cluster_centers_ #获取聚类中心
-print(centroids)
-inertia = estimator.inertia_ # 获取聚类准则的总和
-np.savetxt('gray_40.csv', label_pred, delimiter = ',')
-np.savetxt('gray_centroids_40.csv', centroids, delimiter = ',')
+# estimator = KMeans(n_clusters=40,max_iter=5000)#构造聚类器
+# estimator.fit(data)#聚类
+# label_pred = estimator.labels_ #获取聚类标签
+# print(label_pred)
+# centroids = estimator.cluster_centers_ #获取聚类中心
+# print(centroids)
+# inertia = estimator.inertia_ # 获取聚类准则的总和
+
+centroids, label_pred = vq.kmeans2( data, 44 )
+
+np.savetxt('gray_44.csv', label_pred, delimiter = ',')
+np.savetxt('gray_centroids_44.csv', centroids, delimiter = ',')
 
 
