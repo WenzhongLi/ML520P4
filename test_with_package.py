dce322f5e4f15639210cec2ecf5a4ae6fe5151fa
TobyGE <15188313009@163.com>
2017-12-10 15:31:43 -0500

    update6

--- test_with_package.py
+++ test_with_package.py
@@ -7,14 +7,14 @@ data = np.loadtxt(open("input.csv","rb"),delimiter=",",skiprows=0)
 
 print(type(data))
 
-estimator = KMeans(n_clusters=500,max_iter=5000)#构造聚类器
+estimator = KMeans(n_clusters=40,max_iter=5000)#构造聚类器
 estimator.fit(data)#聚类
 label_pred = estimator.labels_ #获取聚类标签
 print(label_pred)
 centroids = estimator.cluster_centers_ #获取聚类中心
 print(centroids)
 inertia = estimator.inertia_ # 获取聚类准则的总和
-np.savetxt('gray_500.csv', label_pred, delimiter = ',')
-np.savetxt('gray_centroids_500.csv', centroids, delimiter = ',')
+np.savetxt('gray_40.csv', label_pred, delimiter = ',')
+np.savetxt('gray_centroids_40.csv', centroids, delimiter = ',')
 
 
