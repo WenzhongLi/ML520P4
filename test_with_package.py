632d0fb907b57ab9b5bc28cbe27b1a9da9af2647
TobyGE <15188313009@163.com>
2017-12-10 14:52:22 -0500

    update6

--- test_with_package.py
+++ test_with_package.py
@@ -3,17 +3,18 @@
 
 import numpy as np
 from sklearn.cluster import KMeans
-data = np.loadtxt(open("color.csv","rb"),delimiter=",",skiprows=0)
+data = np.loadtxt(open("input.csv","rb"),delimiter=",",skiprows=0)
 
 print(type(data))
 
-estimator = KMeans(n_clusters=60,max_iter=1000)#构造聚类器
+estimator = KMeans(n_clusters=500,max_iter=5000)#构造聚类器
 estimator.fit(data)#聚类
 label_pred = estimator.labels_ #获取聚类标签
 print(label_pred)
 centroids = estimator.cluster_centers_ #获取聚类中心
 print(centroids)
 inertia = estimator.inertia_ # 获取聚类准则的总和
-np.savetxt('rgb_60.csv', label_pred, delimiter = ',')
-np.savetxt('rgb_centroids_60.csv', centroids, delimiter = ',')
+np.savetxt('gray_500.csv', label_pred, delimiter = ',')
+np.savetxt('gray_centroids_500.csv', centroids, delimiter = ',')
+
 
